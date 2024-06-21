4c85b18c197b059210743082952e521bcb45d3b8
hairybear-21566 <hairbear21566@gmail.com>
2024-06-21 14:34:34 +0400

    glad & glfw initialization

--- ChessEngineMain.cpp
+++ ChessEngineMain.cpp
@@ -1,65 +1,51 @@
 // #include <Cocoa/Cocoa.h>
 
-// #include <GLFW/glfw3.h>
 // #include <ChessEngine.h>
 
 #include <iostream>
 #include <vector>
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
+
 using namespace std;
-
-void printVect(vector<int> v)
-{
-    for (int i = 0; i < v.size(); i++)
-    {
-        cout << v[i] << endl;
-    }
-}
-
-class Solution
-{
-public:
-    vector<int> merge(vector<int> nums1, int m, vector<int> nums2, int n)
-    {
-        int i1 = m - 1;
-        int i2 = n - 1;
-        int k = m + n - 1;
-        while (k > -1)
-        {
-            if (nums1[i1] > nums2[i2])
-            {
-                nums1[k] = nums1[i1];
-                i1--;
-            }
-            else
-            {
-                nums1[k] = nums2[i2];
-                i2--;
-            }
-            k--;
-            printVect(nums1);
-            cout << "############" << endl;
-        }
-
-        return nums1;
-    }
-};
 
 int main()
 {
-    // s
-    Solution s;
-    vector<int> n1(6);
-    n1[0] = 1;
-    n1[1] = 2;
-    n1[2] = 3;
-    n1[3] = 0;
-    n1[4] = 0;
-    n1[5] = 0;
-    vector<int> n2(3); // Allocate space for 3 elements
-    n2[0] = 2;
-    n2[1] = 5;
-    n2[2] = 6;
-    vector<int> nums = s.merge(n1, 3, n2, 3);
-    printVect(nums);
+    // initialize GLFW
+    glfwInit();
+    // glfw window hints determine which version we use
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+    // idk
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // for mac
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    GLFWwindow *window = glfwCreateWindow(800, 600, "Chess Engine", NULL, NULL);
+    if (window == NULL)
+    {
+        cout << "Failed to create GLFW window" << endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    // glad handles functions pointers
+    // checks whether glad was initialized properly
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    // set the viewport
+    // glViewport(x, y, width, height)
+    // this tell OpenGL the dimensions of the window we are rendering so this is just full screen
+    glViewport(0, 0, 800, 600);
+
+    void framebuffer_size_callback(GLFWwindow * window, int width, int height){
+        glViewport(0, 0, width, height);
+    }
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); 
+
     return 0;
 }
