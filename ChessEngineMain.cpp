8c92e91dd3c84cf040cf886350c67cdfc79c687c
hairybear-21566 <hairbear21566@gmail.com>
2024-07-26 22:49:16 +0400

    triangle

--- ChessEngineMain.cpp
+++ ChessEngineMain.cpp
@@ -1,51 +1,195 @@
-// #include <Cocoa/Cocoa.h>
 
-// #include <ChessEngine.h>
-
-#include <iostream>
-#include <vector>
-#include <GLFW/glfw3.h>
 #include <glad/glad.h>
 
-using namespace std;
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+#include <vector>
+#ifdef __APPLE__
+#define glGenVertexArrays glGenVertexArraysAPPLE
+#define glBindVertexArray glBindVertexArrayAPPLE
+#define glDeleteVertexArrays glDeleteVertexArraysAPPLE
+#endif
+//#define GL_GLEXT_PROTOTYPES 1
+//#include <SDL2/SDL_opengl.h>
+
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+float vertices[] = {
+    -0.5f,
+    -0.5f,
+    0.0f,
+    0.5f,
+    -0.5f,
+    0.0f,
+    0.0f,
+    0.5f,
+    0.0f,
+};
+
+const char *vertexShaderSource = "#version 330 core\n"
+                                     "layout (location = 0) in vec3 aPos;\n"
+                                     "void main()\n"
+                                     "{\n"
+                                     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+                                     "}\0";
+
+const char *fragmentShaderSource = "#version 330 core\n"
+                                       "out vec4 FragColor;\n"
+                                       "void main()\n"
+                                       "{\n"
+                                       "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+                                       "}\n\0";
+
+// Vertex shader -> vertex 3D coordinates transformation
+// Geometry shader -> takes primitives (points, lines, triangles) and modifies, delete, creates them
+// Shape Assembly ->
+// Rasterization -> maps primitives to pixels (also has clipping to remove primitives/fragments outside of screen)
+// Fragment shader -> takes pixel color and modifies it, gives final color
+// Tests and Blending -> checks the corresponding depth (and stencil) value (we'll get to those later) of the
+//                       fragment and uses those to check if the resulting fragment is in front
+//                       or behind other objects and should be discarded accordingly. The stage also
+//                       checks for alpha values (alpha values define the opacity of an object) and blends
+//                       the objects accordingly
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+void render(GLFWwindow *window)
+{
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
 
 int main()
 {
-    // initialize GLFW
-    glfwInit();
-    // glfw window hints determine which version we use
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
-    // idk
+    // Initialize GLFW
+    if (!glfwInit())
+    {
+        std::cerr << "Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    // Set GLFW window hints for OpenGL version and profile
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    // for mac
+    // For macOS
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    GLFWwindow *window = glfwCreateWindow(800, 600, "Chess Engine", NULL, NULL);
+    // Create GLFW window
+    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "Chess Engine", NULL, NULL);
     if (window == NULL)
     {
-        cout << "Failed to create GLFW window" << endl;
+        std::cerr << "Failed to create GLFW window" << std::endl;
         glfwTerminate();
         return -1;
     }
+    glfwMakeContextCurrent(window);
 
-    // glad handles functions pointers
-    // checks whether glad was initialized properly
+    // Initialize GLAD
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
-        std::cout << "Failed to initialize GLAD" << std::endl;
+        std::cerr << "Failed to initialize GLAD" << std::endl;
         return -1;
     }
 
-    // set the viewport
-    // glViewport(x, y, width, height)
-    // this tell OpenGL the dimensions of the window we are rendering so this is just full screen
-    glViewport(0, 0, 800, 600);
+    // Set the viewport
+    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
-    void framebuffer_size_callback(GLFWwindow * window, int width, int height){
-        glViewport(0, 0, width, height);
+    /* Vertex Setting */
+    // Create vertex buffer object (VBO) and vertex array object (VAO)
+    // VBO is the buffer that holds the vertices in GPU's Memory
+
+    // unsigned int VBO; // vertex buffer object
+    GLuint VBO, VAO;
+    glGenVertexArrays(1, &VAO);
+
+    glGenVertexArrays(VBO);
+    glGenBuffers(1, &VBO); // generate buffer
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    /* Vertex Shader */
+    
+    // for compile vertex shader
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+
+    if (!success)
+    {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+                  << infoLog << std::endl;
     }
-    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback); 
 
+    /* Fragment Shader */
+    
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    /* Link Shader Program */
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success)
+    {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n"<< infoLog << std::endl;
+    }
+
+    glUseProgram(shaderProgram);
+
+    // linked shader no longer needed
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    // Linking Vertex Attribute (tell OpenGL how to interpret bit sequence)
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof(float),(void*)0);
+    glEnableVertexAttribArray(0);
+
+        // Render loop
+        while (!glfwWindowShouldClose(window))
+    {
+        // glClear(GL_COLOR_BUFFER_BIT);
+       
+        processInput(window);
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        render(window);
+
+        // Swap buffers and poll events
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Terminate GLFW
+    glfwTerminate();
     return 0;
 }
